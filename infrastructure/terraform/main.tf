8842832acaabe701566b458af008b52d4b16a5bb
Alandis Seals <1seals.seals@gmail.com>
2025-10-22 11:11:31 -0500

    add dynamo table for visitor counter

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -211,3 +211,22 @@ resource "aws_acm_certificate" "resume_cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_dynamodb_table" "visit_counter" {
+  name        = "${var.project_name}-visit-counter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "couter_id"
+
+  attribute {
+    name = "couter_id"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = {
+    Name = "Visit Counter Table"
+  }
+}

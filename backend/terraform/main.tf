89c84c5f497c919b8a9c5260ffc8bb896d991563
Alandis Seals <1seals.seals@gmail.com>
2025-10-28 20:10:07 -0500

    add HTTP API Gateway

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -79,3 +79,36 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_attach" {
 data "aws_iam_policy" "lambda_basic_execution" {
   name = "AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_apigatewayv2_api" "visitor_api" {
+  name          = "${var.project_name}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.visitor_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.visitor_counter.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "visitor_route" {
+  api_id    = aws_apigatewayv2_api.visitor_api.id
+  route_key = "GET /visits"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "dev_stage" {
+  api_id      = aws_apigatewayv2_api.visitor_api.id
+  name        = "dev"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.visitor_api.execution_arn}/*/*"
+}
